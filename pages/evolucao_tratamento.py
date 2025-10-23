ee7475f59ac4584ae5d4c0349e50caecb15cf21c
Lúcio Rêgo <56084002+lmatheus-rego@users.noreply.github.com>
2025-10-23 20:24:23 +0000

    atualizacao

--- pages/evolucao_tratamento.py
+++ pages/evolucao_tratamento.py
@@ -75,8 +75,8 @@ if not id_paciente_str:
 paciente_info = carregar_paciente(id_paciente_str, sh)
 
 # ----------------- Exibir Dados do Paciente -----------------
-st.markdown("<h3 style='text-align:center;'>📋Cadastro de Evolução: </h3><hr>", unsafe_allow_html=True)
-with st.expander("⬇️ Dados do Paciente ⬇️ ", expanded=False):
+st.markdown("<h3 style='text-align:center;'>📋Cadastro de Evolução: </h3><hr><br>", unsafe_allow_html=True)
+with st.expander(f"⬇️ Dados do Paciente ⬇️  |  {paciente_info.get('NOME', 'Nome não cadastrado')}", expanded=False):
     status = paciente_info.get('STATUS','').strip().lower()
     status_emoji = {"ativo": "✅", "inativo": "⛔", "ausente": "🕓"}.get(status, "❔")
     status_color = {"ativo": "#28a745", "inativo": "#6c757d", "ausente": "#ffc107"}.get(status, "#000")
