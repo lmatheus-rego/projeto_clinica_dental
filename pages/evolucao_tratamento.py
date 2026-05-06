dd85dc38f9b5680949ee39c2fb603de3da379544
Lúcio Rêgo <56084002+lmatheus-rego@users.noreply.github.com>
2026-05-06 18:16:47 +0000

    atualizacao

--- pages/evolucao_tratamento.py
+++ pages/evolucao_tratamento.py
@@ -140,7 +140,7 @@ with st.sidebar:
 # ==========================
 st.markdown("""
     <div class="main-header">
-        <h1>🦷 Cadastrar Evolução</h1>
+        <h1>🦷 Evolução no Tratamento</h1>
     </div>
 """, unsafe_allow_html=True)
 
